f0d19d8dcd36c7b6e250b320854b2919963b14c4
surfer <xxxx@yahoo.com>
2019-05-14 22:50:42 +0800

    add twitter

--- src/main/java/Twitter/Twitter.java
+++ src/main/java/Twitter/Twitter.java
@@ -1,6 +1,7 @@
 package Twitter;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /*
 355. Design Twitter
@@ -42,29 +43,22 @@ twitter.unfollow(1, 2);
 // since user 1 is no longer following user 2.
 twitter.getNewsFeed(1);
  */
-public class Twitter {
 
-    class Tweet implements Comparable<Tweet> {
+public class Twitter {
+    class Tweet {
         private long timestamp;
         private int val;
-
         public Tweet(int val) {
             this.val = val;
-            this.timestamp = System.currentTimeMillis();
+            this.timestamp = System.nanoTime();
         }
-
         public int getVal() { return this.val; }
 
         public long getTimestamp() { return this.timestamp; }
-
-        @Override
-        public int compareTo(Tweet o) {
-            return -Long.valueOf(this.timestamp).compareTo(o.getTimestamp());
-        }
     }
 
     Map<Integer, List<Tweet>> userTweets;
-    Map<Integer, PriorityQueue<Tweet>> userFeeds;
+    Map<Integer, LinkedList<Tweet>> userFeeds;
     Map<Integer, Set<Integer>> userRelations;
 
     /** Initialize your data structure here. */
@@ -79,15 +73,14 @@ public class Twitter {
         Tweet tweet = new Tweet(tweetId);
         if (userTweets.containsKey(userId)) {
             userTweets.get(userId).add(tweet);
-            userFeeds.get(userId).offer(tweet);
+            userFeeds.get(userId).addFirst(tweet);
         }
         else {
             userTweets.put(userId, new LinkedList<>(Collections.singleton(tweet)));
-            userFeeds.put(userId, new PriorityQueue<>(Collections.singleton(tweet)));
+            userFeeds.put(userId, new LinkedList<>(Collections.singleton(tweet)));
             userRelations.put(userId, new HashSet<>());
         }
-
-        for (int follower:userRelations.get(userId)) userFeeds.get(follower).offer(tweet);
+        for (int follower:userRelations.get(userId)) userFeeds.get(follower).addFirst(tweet);
     }
 
     /** Retrieve the 10 most recent tweet ids in the user's news feed.
@@ -98,46 +91,59 @@ public class Twitter {
 
         if (!userFeeds.containsKey(userId)) return tweetList;
 
-        PriorityQueue<Tweet> tweetPQ = userFeeds.get(userId);
-        Iterator<Tweet> tweetIterator = tweetPQ.iterator();
+        List<Tweet> tweets = userFeeds.get(userId);
+        //less efficient but elegant way
+        //return (tweets.size()>10?tweets.subList(0,10):tweets).stream().map(Tweet::getVal).collect(Collectors.toList());
+        Iterator<Tweet> tweetIterator = tweets.iterator();
 
         for (int i=0;i<10 && tweetIterator.hasNext();++i) tweetList.add(tweetIterator.next().getVal());
-
-        return tweetList;
+        return tweetList.subList(0,10);
     }
 
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
+        if (followeeId==followerId) return;
         if (!userFeeds.containsKey(followerId)) createUser(followerId);
         if (!userTweets.containsKey(followeeId)) createUser(followeeId);
-        userRelations.get(followeeId).add(followerId);
-        userFeeds.get(followerId).addAll(userTweets.get(followeeId));
+
+        if (userRelations.get(followeeId).add(followerId)) {
+            userFeeds.get(followerId).addAll(userTweets.get(followeeId));
+            userFeeds.get(followerId).sort(Comparator.comparingLong(Tweet::getTimestamp).reversed());
+        }
     }
 
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
+        if (followeeId==followerId) return;
         if (!userFeeds.containsKey(followerId)) createUser(followerId);
         if (!userTweets.containsKey(followeeId)) createUser(followeeId);
-        userRelations.get(followeeId).remove(followerId);
-        userFeeds.get(followerId).removeAll(userTweets.get(followeeId));
+
+        if (userRelations.get(followeeId).remove(followerId)) {
+            userFeeds.get(followerId).removeAll(userTweets.get(followeeId));
+        }
     }
 
     private void createUser(int userId) {
         userTweets.put(userId, new LinkedList<>());
-        userFeeds.put(userId, new PriorityQueue<>());
+        userFeeds.put(userId, new LinkedList<>());
         userRelations.put(userId, new HashSet<>());
     }
 
-    /**
-     * Your Twitter object will be instantiated and called as such:
-     * Twitter obj = new Twitter();
-     * obj.postTweet(userId,tweetId);
-     * List<Integer> param_2 = obj.getNewsFeed(userId);
-     * obj.follow(followerId,followeeId);
-     * obj.unfollow(followerId,followeeId);
-     */
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Twitter t = new Twitter();
+        t.postTweet(1,4);
+        t.postTweet(2,5);
+        t.unfollow(1,2);
+        t.follow(1,2);
+        for (int tweet:t.getNewsFeed(1)) System.out.println("tweet = " + tweet + ",");
     }
 }
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
